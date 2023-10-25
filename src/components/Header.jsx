369223caed59ae3f2c7c2734847596f102772439
Ripty-Dx <riptyfullstack.dx@gmail.com>
2023-10-25 10:12:22 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return <>
+  <div className="container">
+    <div className="row">
+         <div className="d-flex ">
+
+         </div>
+    </div>
+  </div>
+  </>
+}
+
+export default Header
